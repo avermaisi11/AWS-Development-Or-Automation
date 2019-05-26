68fb50e139a85e935690b3ea8c062e9117818774
Pankaj Mandal <pankajmandal361@gmail.com>
2019-05-26 16:47:25 +0530

    Deregister old AMIs

---
+++ deregisterOldAMIs.py
@@ -0,0 +1,34 @@
+import datetime
+from dateutil.parser import parse
+import boto3
+
+def days_old(date):
+	parsed = parse(date).replace(tzinfo=None)
+	diff = datetime.datetime.now() - parsed
+
+	return diff.days
+
+def lambda_handler(event, context):
+	ec2_client = boto3.client('ec2')
+	regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
+	
+	#Fetching all the regions
+	for region in regions:
+		ec2 = boto3.client('ec2', region_name = region)
+		print("Region: ", region)
+		
+		#Fetching self owned images
+		amis = ec2.describe_images(Owners=['self'])['Images']
+		
+		#Iterating throug amis
+		for ami in amis:
+			creation_date = ami['CreationDate']
+			age_days = days_old(creation_date)
+			image_id = ami['ImageId']
+			print('ImageId: {}, CreationDate: {} ({} days old)'. format(image_id, creation_date, age_days))
+
+			if age_days >= 2 :
+				print('Deleting ImageId: ', image_id)
+				#Deregister Images
+				ec2.deregister_image(ImageId = image_id)
+
