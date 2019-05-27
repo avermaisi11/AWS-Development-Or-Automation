e61887f27e3a0f4a15cc31389f7153f617525df8
Pankaj Mandal <pankajmandal361@gmail.com>
2019-05-27 12:36:42 +0530

    recognizing faces from images

---
+++ POC On AWS Services Using Python/face_rekognition_automation.py
@@ -0,0 +1,40 @@
+import os, boto3
+
+TABLE_NAME = os.environ['TABLE_NAME']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(TABLE_NAME)
+
+s3 = boto3.resource('s3')
+
+rekognition = boto3.client('rekognition')
+
+def lambda_handler(event, context):
+	#Fetching bucket name
+	bucket = event['Records'][0]['s3']['bucket']['name']
+	key = event['Records'][0]['s3']['object']['key']
+
+	obj = s3.Object(bucket, key)
+	image = obj.get()['Body'].read()
+
+	print('Rekognition celebrities...')
+	response = rekognition.recognize_celebrities(Image={'Bytes':image})
+
+	names=[]
+
+	for celebrity in response['CelebrityFaces']:
+		name = celebrity['Name']
+		print('Name: ' + name)
+		names.append(name)
+
+	print(names)
+
+	print('Saving face data to dynamodb table: ',TABLE_NAME)
+
+	response = table.put_item(
+			Item={
+				'key': key,
+				'names' names
+			}
+		)
+
+	print(response)
